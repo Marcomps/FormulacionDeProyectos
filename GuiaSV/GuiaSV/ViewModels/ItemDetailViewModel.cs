a965f2f02b0cd39bf23d7df8ae56f4d7b0e833d2
Marco Mendez <marcomendez1796@hotmail.com>
2020-06-04 22:53:56 -0600

    Proyecto Base

---
+++ GuiaSV/GuiaSV/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using GuiaSV.Models;
+
+namespace GuiaSV.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
